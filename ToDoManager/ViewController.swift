e7b5c7650b43faaf868d2019a6eee2c4bc2fce6c
Nataliya <nataliya.laz@gmail.com>
2022-08-09 09:37:52 +0300

    Initial Commit

---
+++ ToDoManager/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoManager
+//
+//  Created by Nataliya Lazouskaya on 9.08.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
